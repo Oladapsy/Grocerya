161acfad1c67e2ee8e0e241549e6f61b9753f9ef
oladapsy <dapoodedeyi@yahoo.com>
2026-02-24 11:00:28 +0100

    screens for the tabs

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { View, Text } from 'react-native';
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View>
+      <Text>Profile Screen</Text>
+    </View>
+  );
+}
+
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
